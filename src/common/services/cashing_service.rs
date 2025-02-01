ebd0a13601c788d416bca2df4d880c0c41751873
Filip Barabas <filip.barabas@student.tuke.sk>
2025-02-01 22:05:13 +0100

    Add variable to enable/disable cashing

--- src/common/services/cashing_service.rs
+++ src/common/services/cashing_service.rs
@@ -1,16 +1,25 @@
-use crate::common::database::Database;
+use crate::common::{database::Database, helper::get_env_variable};
 use redis::AsyncCommands;
 use serde::{de::DeserializeOwned, Serialize};
 
 #[derive(Clone)]
 pub struct CashingService {}
- 
+
 impl CashingService {
     pub fn new() -> Self {
         Self {}
     }
 
+    pub fn cashing_enabled(&self) -> bool {
+        let enabled_env = get_env_variable("ALLOW_CASHING");
+        enabled_env == "true"
+    }
+
     pub async fn get<T: DeserializeOwned>(&self, key: &str) -> anyhow::Result<Option<T>> {
+        if !self.cashing_enabled() {
+            return Ok(None);
+        }
+
         let mut redis_conn = Database::redis().await;
         let cached: Option<Vec<u8>> = redis_conn.get(key).await?;
 
@@ -21,6 +30,10 @@ impl CashingService {
     }
 
     pub async fn set<T: Serialize>(&self, key: String, value: &T) -> anyhow::Result<()> {
+        if !self.cashing_enabled() {
+            return Ok(());
+        }
+
         let mut redis_conn = Database::redis().await;
         let value_string = rmp_serde::to_vec(value)?;
 
