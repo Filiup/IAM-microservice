9f2f9cfa3841e459b05d237223029ac3e3ff432f
Filip Barabas <filip.barabas@student.tuke.sk>
2025-02-04 22:57:32 +0100

    Add gRPC controller implementation

--- src/iam/iam_grpc_controller.rs
+++ src/iam/iam_grpc_controller.rs
@@ -1,8 +1,9 @@
-use poem_grpc::{Request, Response, Status};
+use poem_grpc::{Code, Request, Response, Status};
+
+use crate::{common::security::jwt_decoder, iam::controller_common};
 
 use super::iam_model::{AclAction, AclMessage};
 poem_grpc::include_proto!("iam");
-
 pub struct IamGrpcController;
 
 impl Into<AclAction> for AclActionRpc {
@@ -12,6 +13,17 @@ impl Into<AclAction> for AclActionRpc {
             AclActionRpc::Delete => AclAction::DELETE,
             AclActionRpc::Edit => AclAction::EDIT,
             AclActionRpc::Read => AclAction::READ,
+        }
+    }
+}
+
+impl Into<AclActionRpc> for AclAction {
+    fn into(self) -> AclActionRpc {
+        match self {
+            AclAction::ACCESS => AclActionRpc::Access,
+            AclAction::DELETE => AclActionRpc::Delete,
+            AclAction::READ => AclActionRpc::Read,
+            AclAction::EDIT => AclActionRpc::Edit,
         }
     }
 }
@@ -27,11 +39,43 @@ impl Into<AclMessage> for AclMessageRpc {
     }
 }
 
+impl Into<AclMessageRpc> for AclMessage {
+    fn into(self) -> AclMessageRpc {
+        let operation_rpc: AclActionRpc = self.operation.into();
+
+        AclMessageRpc {
+            allowed: self.allowed,
+            client_alias_ids: self.client_alias_ids,
+            operation: operation_rpc.into(),
+            permission: self.permission,
+        }
+    }
+}
+
 impl IamService for IamGrpcController {
     async fn get_access_rights(
         &self,
-        _request: Request<AclRequestRpc>,
+        request: Request<AclRequestRpc>,
     ) -> Result<Response<AclResponseRpc>, Status> {
-        todo!()
+        let jwt_public_key = ""; // TODO
+
+        let authorization_header = request.metadata().get("authorization").ok_or(
+            Status::new(Code::Unauthenticated)
+                .with_message("You must provide authorization header with JWT token."),
+        )?;
+
+        let user = jwt_decoder::decode_jwt(authorization_header, jwt_public_key).ok_or(
+            Status::new(Code::Unauthenticated).with_message("Failed to decode provided JWT."),
+        )?;
+
+        let rights_rpc = request.into_inner().rights;
+        let rights: Vec<AclMessage> = rights_rpc.into_iter().map(|m| m.into()).collect();
+
+        let result = controller_common::get_permissions(rights, user.caid, user.gid)
+            .await
+            .map_err(|e| Status::new(Code::Internal).with_message(e.to_string()))?;
+
+        let result_rpc: Vec<AclMessageRpc> = result.into_iter().map(|m| m.into()).collect();
+        Ok(Response::new(AclResponseRpc { rights: result_rpc }))
     }
 }
