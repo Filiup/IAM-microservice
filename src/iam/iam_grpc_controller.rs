37fd32e598efcbc2118aba1226724c65ea8f8bc6
Filip Barabas <filip.barabas@student.tuke.sk>
2025-02-04 21:53:37 +0100

    Add Into trait implementations to convert RPC structs to module structs

--- src/iam/iam_grpc_controller.rs
+++ src/iam/iam_grpc_controller.rs
@@ -1,7 +1,31 @@
 use poem_grpc::{Request, Response, Status};
+
+use super::iam_model::{AclAction, AclMessage};
 poem_grpc::include_proto!("iam");
 
 pub struct IamGrpcController;
+
+impl Into<AclAction> for AclActionRpc {
+    fn into(self) -> AclAction {
+        match self {
+            AclActionRpc::Access => AclAction::ACCESS,
+            AclActionRpc::Delete => AclAction::DELETE,
+            AclActionRpc::Edit => AclAction::EDIT,
+            AclActionRpc::Read => AclAction::READ,
+        }
+    }
+}
+
+impl Into<AclMessage> for AclMessageRpc {
+    fn into(self) -> AclMessage {
+        AclMessage {
+            allowed: self.allowed,
+            client_alias_ids: self.client_alias_ids.clone(),
+            operation: self.operation().into(),
+            permission: self.permission,
+        }
+    }
+}
 
 impl IamService for IamGrpcController {
     async fn get_access_rights(
